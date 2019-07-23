df79f268185ebf4d841df44805c06cfc6dce8d37
Łukasz Harasimowicz <dev@harnash.eu>
2019-07-23 19:29:59 +0200

    Added basic command arguments and flags

--- main.go
+++ main.go
@@ -2,36 +2,49 @@ package main
 
 import (
 	"os"
+	"strings"
 
 	"github.com/harnash/goversion/pkg/config"
 
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-var (
-	app     = kingpin.New("goversion", "semantic versioning tool").Version("0.0.1")
-	verbose = app.Flag("verbose", "Enable verbose mode").Bool()
-
-	patch = app.Command("patch", "increments patch component of semantic version")
-	minor = app.Command("minor", "increments minor component of semantic version")
-	major = app.Command("major", "increments major component of semantic version")
-)
-
 func main() {
 	cfg, _ := config.NewFromEnv()
 
-	cmd := kingpin.MustParse(app.Parse(os.Args[1:]))
-	cfg.VerboseMode = *verbose
+	app := kingpin.New("goversion", "semantic versioning tool").Version("0.0.1")
+	app.Flag("verbose", "enable verbose mode").BoolVar(&cfg.VerboseMode)
 
-	switch cmd {
-	case patch.FullCommand():
-		println("patch")
-	case minor.FullCommand():
-		println("minor")
-	case major.FullCommand():
-		println("major")
-	}
+	var noTag bool
+	var noCommit bool
+
+	app.Flag("dry-run", "enable dry-run mode (do not apply changes, use with 'verbose' mode").
+		BoolVar(&cfg.DryRunMode)
+	app.Flag("tag", "version will be tagged if possible (git/mercurial)").
+		BoolVar(&cfg.TagVersion)
+	app.Flag("no-tag", "should not create tag for new version").
+		BoolVar(&noTag)
+	app.Flag("allow-dirty", "proceed with bumping version even if repo has uncommitted changes").
+		BoolVar(&cfg.AllowDirty)
+	app.Flag("parse", "regex used to parse version string").StringVar(&cfg.ParseTemplate)
+	app.Flag("serialize", "format used to print version string").StringsVar(&cfg.SerializeTemplate)
+	app.Flag("commit", "should commit changes when finish").BoolVar(&cfg.CommitVersion)
+	app.Flag("no-commit", "should not commit changes when finish").BoolVar(&noCommit)
+	app.Flag("message", "template for message of the commit").StringVar(&cfg.CommitMessage)
+	app.Flag("new-version", "new version that should be set").StringVar(&cfg.NewVersion)
+
+	parts := app.Arg("part", "part of the version that should be bumped").Enum("patch", "minor", "major")
+	files := app.Arg("files", "files to process").ExistingFiles()
+
+	_ = kingpin.MustParse(app.Parse(os.Args[1:]))
+
+	println(*parts)
+	println(strings.Join(*files, "\n"))
+
 	if cfg.VerboseMode {
 		println("verbose mode on")
+	}
+	if cfg.TagVersion {
+		println("tag version")
 	}
 }
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,13 +1,33 @@
 package config
 
 type Configuration struct {
-	CurrentVersion string
-	NewVersion     string
-	CommitVersion  bool
-	CommitMessage  string
-	TagVersion     bool
-	TagName        string
-	VerboseMode    bool
+	CurrentVersion    string                 `yaml:"current_version"`
+	NewVersion        string                 `yaml:"new_version"`
+	CommitVersion     bool                   `yaml:"commit"`
+	CommitMessage     string                 `yaml:"commit_message"`
+	TagVersion        bool                   `yaml:"tag"`
+	TagName           string                 `yaml:"tag_name"`
+	VerboseMode       bool                   `yaml:"verbose"`
+	DryRunMode        bool                   `yaml:"dry_run"`
+	AllowDirty        bool                   `yaml:"allow_dirty"`
+	List              bool                   `yaml:"list"`
+	SerializeTemplate []string               `yaml:"serialize"`
+	ParseTemplate     string                 `yaml:"parse"`
+	ReleaseParts      map[string]ReleasePart `yaml:"parts"`
+	ReleaseFiles      map[string]ReleaseFile `yaml:"files"`
+}
+
+type ReleasePart struct {
+	OptionalValue string   `yaml:"optional_value"`
+	Values        []string `yaml:"values"`
+	FirstValue    string   `yaml:"first_value"`
+}
+
+type ReleaseFile struct {
+	Search            string   `yaml:"search"`
+	Replace           string   `yaml:"replace"`
+	ParseTemplate     string   `yaml:"parse"`
+	SerializeTemplate []string `yaml:"serialize"`
 }
 
 func NewFromEnv() (*Configuration, error) {
